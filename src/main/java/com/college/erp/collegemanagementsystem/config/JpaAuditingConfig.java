423cf87ee12b92def22ed62934ddb0fbc5cde92b
grish <grishshrestha2@gmail.com>
2026-06-27 23:18:03 +0545

    JpaAuditingConfig: Added DateTimeProvider bean

--- src/main/java/com/college/erp/collegemanagementsystem/config/JpaAuditingConfig.java
+++ src/main/java/com/college/erp/collegemanagementsystem/config/JpaAuditingConfig.java
@@ -1,9 +1,18 @@
 package com.college.erp.collegemanagementsystem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "offsetDateTimeProvider")
 public class JpaAuditingConfig {
+    @Bean
+    public DateTimeProvider offsetDateTimeProvider() {
+        return () -> Optional.of(OffsetDateTime.now());
+    }
 }
