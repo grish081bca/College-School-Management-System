346813c73d92bb06a99e9840dd8c87123ee30e28
grish <grishshrestha2@gmail.com>
2026-06-27 22:27:51 +0545

    Configuration Added for Auditing with JpaAuditingConfig

---
+++ src/main/java/com/college/erp/collegemanagementsystem/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.college.erp.collegemanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
